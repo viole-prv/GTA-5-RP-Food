a21a59641aa8bcb0d37048ea2673bd9fb850fbf6
Viole <xtakolp1337@gmail.com>
2024-10-31 22:57:40 +0300

    refactor: update prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,25 @@
 module.exports = {
-  parser:  "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.json",
-  },
-  plugins: [
-    "simple-import-sort",
-    "import"
-  ],
-  rules: {
-    "import/newline-after-import": ["error", { "count": 1 }],
-    "simple-import-sort/exports": "error",
-    "simple-import-sort/imports": ["error", { groups: [
-        ["^react", "^@?\\w"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: "./tsconfig.json",
+    },
+    plugins: ["simple-import-sort", "import"],
+    rules: {
+        "import/newline-after-import": ["error", { count: 1 }],
+        "simple-import-sort/exports": "error",
+        "simple-import-sort/imports": [
+            "error",
+            {
+                groups: [
+                    ["^react", "^@?\\w"],
 
-        ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+                    ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
 
-        ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+                    ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
 
-        ["^.+\\.s?css$"],
-      ] }]
-  },
-}
+                    ["^.+\\.s?css$"],
+                ],
+            },
+        ],
+    },
+};
